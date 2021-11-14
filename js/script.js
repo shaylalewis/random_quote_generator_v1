92b60a93d8d0f391c70d9e16e9c5eec662f9d890
Crime <62856686+shaylalewis@users.noreply.github.com>
2021-11-14 00:43:27 -0500

    Added files via upload
    Added files for the random quote generator

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/************************************************
+Treehouse Techdegree:
+Project 4 - Random Quote Generator
+*************************************************/
+
+// For assistance: 
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community
+
+/*** 
+ * `quotes` array 
+ ***/
+let quotes = [
+    { quote: "I can find love, but I can't find money.", source: 'Yuta Nakamoto', citation: 'Buzzfeed Would You Rather Game', year: 2018 },
+    { quote: "Remember, licking doorknobs on other planets is illegal!", source: 'Spongebob', citation: "One Krabs Trash", year: 2002 },
+    { quote: "I lived in America four years that's why I'm here.", source: 'SM Rookies Promo Video', citation: 'Jae Jung', year: 2013 },
+    { quote: 'Meow means woof in cat.', source: 'George Carlin', citation: 'Good Reads', year: 'Unknown' },
+    { quote: "If I looked like that I wouldn't leave the house all day. Not for all the bells in the world", source: 'Opal', citation: 'Animal Crossing Population Growing', year: 2002 }
+];
+
+
+/***
+ * `getRandomQuote` function
+ ***/
+
+function getRandomQuote() {
+    const random = Math.floor(Math.random() * quotes.length);
+    return quotes[random];
+}
+
+console.log(getRandomQuote());
+
+/***
+ * `printQuote` function
+ ***/
+
+function printQuote() {
+
+    const
+
+}
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+ ***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
